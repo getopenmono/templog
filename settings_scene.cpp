9fc12a58d140ec2e1fd7b7000deeb0c6abe4b007
Kristoffer Andersen <stoffera@gmail.com>
2017-01-15 20:43:25 +0100

    Added callbacks to settings

--- settings_scene.cpp
+++ settings_scene.cpp
@@ -4,13 +4,17 @@
 SettingsScene::SettingsScene() :
     timeScaleLbl(Rect(10, 10, 156, 35), "Graph timescale:"),
     ssidLbl(Rect(10, 135, 176, 35), "Wifi: -"),
-    hrs4(Rect(40        , 5+35, 50, 35), "4 hr."),
-    hrs8(Rect(40+50+5   , 5+35, 50, 35), "8 hr."),
-    hrs24(Rect(40       , 5+75, 50, 35), "24 hr."),
-    hrs48(Rect(40+50+5  , 5+75, 50, 35), "48 hr."),
+    hrs4(Rect(40        , 5+35, 50, 35), "4 hr"),
+    hrs8(Rect(40+50+5   , 5+35, 50, 35), "8 hr"),
+    hrs24(Rect(40       , 5+75, 50, 35), "24hr"),
+    hrs48(Rect(40+50+5  , 5+75, 50, 35), "48hr"),
     connectBtn(mono::geo::Rect(20, 160, 176-40, 35), "Connect")
 {
-    
+    hrs4.setClickCallback<SettingsScene>(this, &SettingsScene::hrs4Call);
+    hrs8.setClickCallback<SettingsScene>(this, &SettingsScene::hrs8Call);
+    hrs24.setClickCallback<SettingsScene>(this, &SettingsScene::hrs24Call);
+    hrs48.setClickCallback<SettingsScene>(this, &SettingsScene::hrs48Call);
+    connectBtn.setClickCallback<SettingsScene>(this, &SettingsScene::cnctCall);
 }
 
 void SettingsScene::repaint()
@@ -26,6 +30,8 @@ void SettingsScene::repaint()
     hrs24.scheduleRepaint();
     hrs48.scheduleRepaint();
     connectBtn.scheduleRepaint();
+    
+    
 }
 
 void SettingsScene::show()
@@ -52,4 +58,12 @@ void SettingsScene::hide()
     connectBtn.hide();
     
     scheduleRepaint();
+    
+    async<mbed::FunctionPointer>(&dismissHandler, &mbed::FunctionPointer::call);
 }
+
+void SettingsScene::hrs4Call() { timescaleHandler.call(4); }
+void SettingsScene::hrs8Call() { timescaleHandler.call(8); }
+void SettingsScene::hrs24Call() { timescaleHandler.call(24); }
+void SettingsScene::hrs48Call() { timescaleHandler.call(48); }
+void SettingsScene::cnctCall() { connectHandler.call(); }
--- settings_scene.h
+++ settings_scene.h
@@ -3,16 +3,26 @@
 #define settings_scene_h
 
 #include <mono.h>
+#include <mbed.h>
 
 using namespace mono;
 using namespace mono::geo;
 
 class SettingsScene : public mono::ui::View {
     
+protected:
+    
     mono::ui::TextLabelView timeScaleLbl, ssidLbl;
     mono::ui::ButtonView hrs4, hrs8, hrs24, hrs48, connectBtn;
     
+    void hrs4Call(); void hrs8Call(); void hrs24Call(); void hrs48Call();
+    void cnctCall();
+    
+    
 public:
+    
+    mbed::FunctionPointerArg1<void, int> timescaleHandler;
+    mbed::FunctionPointer connectHandler, dismissHandler;
     
     SettingsScene();
     
